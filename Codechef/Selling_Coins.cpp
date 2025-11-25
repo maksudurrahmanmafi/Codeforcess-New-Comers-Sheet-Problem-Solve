acc66e277f9a7e43399833794f0348533490fd43
Mafi2482 <maksudurrahmanmafi0@gmail.com>
2025-11-25 19:58:01 +0600

    Codechef problem

---
+++ Codechef/Selling_Coins.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int s,g;
+    cin>>s>>g;
+    cout<<(s*1)+(g*2);
+    return 0;
+}
